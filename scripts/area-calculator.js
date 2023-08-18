5368c458ade8ec45c700ba5cf77a80c174147cb5
Md Jobaer Hossain <120457646+jobaer6768@users.noreply.github.com>
2023-08-18 20:54:07 +0600

    added a validation check

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -22,26 +22,45 @@ function area(a, b){
     return a*b;
 }
 
+function validationCheck(b, h){
+    return (isNaN(b) || isNaN(h));
+}
+
 function calculateTriangleArea(){
     const triangleBase = getInputFieldById('triangle-base');
     const triangleHeight = getInputFieldById('triangle-height');
-    const triangleArea = 0.5 * area(triangleBase, triangleHeight);
 
+    if(validationCheck(triangleBase, triangleHeight)){
+        alert('Please enter a number');
+        return;
+    }
+
+    const triangleArea = 0.5 * area(triangleBase, triangleHeight);
     setElement('triangle-area', triangleArea);
 }
 
 function calculateRectangleArea(){
     const width = getInputFieldById('rectangle-width');
     const length = getInputFieldById('rectangle-length');
-    const rectangleArea = area(width, length);
 
+    if(validationCheck(width, length)){
+        alert('Please enter a number');
+        return;
+    }
+
+    const rectangleArea = area(width, length);
     setElement('rectangle-area', rectangleArea);
 }
 
 function calculateParallelogramArea(){
     const base = getInputFieldById('parallelogram-base');
     const height = getInputFieldById('parallelogram-height');
-    const parallelogramArea = area(base, height);
 
+    if(validationCheck(base, height)){
+        alert('Please enter a number');
+        return;
+    }
+
+    const parallelogramArea = area(base, height);
     setElement('parallelogram-area', parallelogramArea);
 }
